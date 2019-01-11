735e3a75f64a6263a7d94f6bfc378a4a03d0966c
Labun Chemjong <labun@whitehatagency.com.au>
2019-01-11 10:34:20 +0545

    gulp-sass and gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function(){
-	return gulp.src('src/scss/app.scss')
-	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-	.pipe(gulp.dest('dist/css'));
+gulp.task('sass', function () {
+   return gulp.src('src/scss/app.scss')
+   .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+   .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('default');
+gulp.task('default', gulp.series('sass'));
